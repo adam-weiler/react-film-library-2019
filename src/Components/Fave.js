cb874402840f630a610c9bb968105fd4379dcc94
adam-weiler <adam.weiler@yahoo.ca>
2019-09-02 09:48:53 -0400

    Fixing Fave.js handleClick event to register isFave state to either true or false. Adding btnFaveStatus to change CSS based on isFave.

--- src/Components/Fave.js
+++ src/Components/Fave.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Fave = ({}) => {
     // console.log('Fave.js renders.');
 
-    //map
+    // isFave is a stateful value. setIsFave is a function to return it. false is the default value.
+    const [isFave, setIsFave] = useState(false);  
+    let btnFaveStatus = 'add_to_queue';
 
-    const handleClick = (event) => {
-        event.stopPropagation();
+
+    const handleClick = (event) => {  // When user clicks on 'Add to faves' button.
+        event.stopPropagation();  // Prevents page from registering 2 events for 1 click.
         console.log('Fave ; handleClick. ');
+        
+        setIsFave(!isFave);  // This toggles the opposite state.
     }
 
+    isFave ? btnFaveStatus = 'remove_from_queue' : btnFaveStatus = 'add_to_queue';  // Changes the btnFaveStatus style depending on if isFave is true or false.
+
     return (
-        <button className="film-row-fave add_to_queue" onClick = { (event) => handleClick(event) }>
+        <button className={ `film-card-fave ${ btnFaveStatus }`}  onClick = { (event) => handleClick(event) }>
             <i className="material-icons">Add to faves</i>
         </button>
     );
